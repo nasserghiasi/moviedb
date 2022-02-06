590b00f50361f4aad8508e5920db900f55c7ffd4
Nasser Ghiasi <nasserghiasi@gmail.com>
2022-02-06 13:49:38 +0330

    feat: routed components

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.scss';
+import { useCallback, useState } from 'react';
+import Input from '../../components/InputField';
+import useSearchMovies from '../../hooks/search-movies';
+import MovieItem from '../../components/MovieItem';
+
+function Home() {
+  const [query, setQuery] = useState('The Lord of the Rings');
+  const { movies } = useSearchMovies(query);
+
+  const handChangeQuery = useCallback(event => {
+    setQuery(event.target.value);
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.searchInputContainer}>
+        <Input value={query} onChange={handChangeQuery} />
+      </div>
+      <div className={styles.results}>
+        {movies.map(movie => (
+          <MovieItem className={styles.movieItem} data={movie} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
