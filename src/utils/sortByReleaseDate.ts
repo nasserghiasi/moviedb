a5b712e4ed0a6fd35764e0b9a6b2e35969251b65
Nasser Ghiasi <nasserghiasi@gmail.com>
2022-02-06 21:30:33 +0330

    feat: sort fav list and watch list by created time

---
+++ src/utils/sortByReleaseDate.ts
@@ -0,0 +1,9 @@
+import { Movie } from '../models/movie';
+
+export default function sortByCreatedDate(movies: Movie[]) {
+  return movies.sort((a, b) =>
+    a.created_at && b.created_at
+      ? new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      : 0
+  );
+}
